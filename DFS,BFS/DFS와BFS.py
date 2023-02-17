3e266bda82f1043c02da0df813b1a571781d8780
hanju <ahj1903@naver.com>
2023-02-18 00:54:01 +0900

    boj 1260 미완성

--- DFS,BFS/DFS와BFS.py
+++ DFS,BFS/DFS와BFS.py
@@ -3,39 +3,18 @@ from collections import deque
 import sys
 sys.stdin = open("C:\\Users\\안한주\\Desktop\\파이썬 코테\\DFS,BFS\\input.txt")
 
-n,m,v = map(int,input().split())
-
 def dfs(v):
-    dfs_visited[v] = 1
-    print(v, end = ' ')
-    for i in graph[v]:
-        if dfs_visited[v] == 0:
-           dfs_visited[i] = 1
-           dfs(i)
+    visit[v] = 1
+    for i in range(1,n+1):
+        if visit[i]==0 and 
 
 
-def bfs(v):
-    q = deque([v])
-    bfs_visited[v] = 1
-    while q:
-        v = q.popleft()
-        print(v, end=" ")
-        for next_node in graph[v]:
-            if bfs_visited[next_node] == 0:
-                bfs_visited[next_node] = 1
-                q.append(next_node)
 
-graph = [[] for _ in range(n+1)]
+
+n,m,v = map(int, input().split())
+
+graph = [[0]*(n+1) for _ in range(n+1)]
+visit = [0]*(n+1)
 
 for i in range(m):
-    a, b = map(int, input().split())
-    graph[a].append(b)  # 양방향 그래프와 낮은 수를 가지는 정점부터 탐색을 시작하기 위함
-    graph[b].append(a)
-    graph[a].sort()
-
-dfs_visited = [0] * (n+1)
-bfs_visited = [0] * (n+1)
-
-dfs(v)
-print()
-bfs(v)
+    array = 

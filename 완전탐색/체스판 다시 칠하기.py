84c82f2515e496bc84d8fa8bdc9e204517f4f1bf
hanju <ahj1903@naver.com>
2023-02-21 00:46:27 +0900

    백준 1018 푸는중

--- 완전탐색/체스판 다시 칠하기.py
+++ 완전탐색/체스판 다시 칠하기.py
@@ -1,14 +1,30 @@
+from collections import deque
 import sys
-sys.stdin = open("C:\\Users\\안한주\\Desktop\\파이썬 코테\\완전탐색\\input.txt")
+sys.stdin = open("C:\\Users\\안한주\\Desktop\\coding_test\\완전탐색\\input.txt")
 
 row, col = map(int, input().split())
+board = [list(map(str,input().strip())) for _ in range(row)]
+visit = [[False]*col for _ in range(row)]
 
-cnt = 0
-for i in range(row):
-    array = list(input())
-    for i in range(col):
-        if array[i] == array[i+1] and i+1<=col:
-            cnt += 1
-        
+dx = [1,-1,0,0]
+dy = [0,0,1,-1]
 
-print(cnt)
+def bfs(x,y):
+    queue = deque()    
+    queue.append((x,y))
+
+    while queue:
+        x,y = queue.popleft()
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if 0 <= nx < col and 0 <= ny < row:
+                if visit[nx][ny] == False:
+                    pass
+
+for i in row:
+    for j in col:
+        if board[i][j]:
+            pass

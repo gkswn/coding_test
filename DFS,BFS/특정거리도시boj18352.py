ce63d232658c324e3cc1c4d9c29a487a6d9b3b66
hanju <ahj1903@naver.com>
2023-03-05 23:53:52 +0900

    풀다 지침

---
+++ DFS,BFS/특정거리도시boj18352.py
@@ -0,0 +1,23 @@
+from collections import deque
+import sys
+sys.stdin = open("C:\\Users\\안한주\\Desktop\\coding_test\\DFS,BFS\\input.txt")
+
+n,m,k,x = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+visit = [-1] *(n+1)
+visit[x] = 0
+
+for i in range(m):
+    x,y = map(int, input().split())
+    graph[x].append(y)
+    
+def bfs(start):
+    answer = []
+    visit[start] = True
+    q = deque()
+    q.append([start])
+    while q:
+        now = q.popleft()
+        for i in range(m):
+
+        

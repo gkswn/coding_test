110c2d81a79e929100bf50d9a83601d6a0de026a
hanju <ahj1903@naver.com>
2023-03-08 02:10:16 +0900

    코드 바꿔리ㅏ

--- 이진탐색/나무자르기boj2805.py
+++ 이진탐색/나무자르기boj2805.py
@@ -3,11 +3,24 @@ sys.stdin = open("C:\\Users\\안한주\\Desktop\\coding_test\\이진탐색\\inpu
 
 n,m = map(int,input().split())
 arr = list(map(int, input().split()))
+arr.sort()
 
-def binary_search(array, target, start, end):
-    while start>=end:
+def binary_search(array, start, end):
+    res = 0
+    while start<=end:
         mid = (start+end)//2
-        
+        total = 0
 
-print(n,m)
-print(arr)
+        for x in array:
+            if x>mid:
+                total += x -mid
+        
+        if total < m:
+            end = mid - 1
+        else:
+            res = mid
+            start = mid+1 
+    return res
+
+r = binary_search(arr,0,max(arr))
+print(r)
